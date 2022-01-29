d9eeb08302590e1bb3318f50de536edb23a0f997
Kikkirej <5462557+Kikkirej@users.noreply.github.com>
2022-01-29 11:28:07 +0100

    🔊

--- src/main/kotlin/net/kikkirej/alexandria/analyzer/maven/MavenAnalysisExecutor.kt
+++ src/main/kotlin/net/kikkirej/alexandria/analyzer/maven/MavenAnalysisExecutor.kt
@@ -5,6 +5,7 @@ import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription
 import org.camunda.bpm.client.task.ExternalTask
 import org.camunda.bpm.client.task.ExternalTaskHandler
 import org.camunda.bpm.client.task.ExternalTaskService
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import java.io.File
@@ -15,10 +16,13 @@ class MavenAnalysisExecutor(@Autowired val generalProperties: GeneralProperties,
                             @Autowired val xmlExtractor: XmlExtractor,
                             @Autowired val dependencyTreeAnalyzer: DependencyTreeAnalyzer,
 ) : ExternalTaskHandler {
+    val log = LoggerFactory.getLogger(javaClass)
     override fun execute(externalTask: ExternalTask?, externalTaskService: ExternalTaskService?) {
         try {
             val modulePath = modulePathOf(externalTask!!)
+            log.info("Analyzing Maven-Module in path:")
             val module = xmlExtractor.getModuleFromXML(modulePath, externalTask.businessKey)
+            log.info("Maven Module identified: %o", module)
             dependencyTreeAnalyzer.analyze(modulePath, module)
             externalTaskService!!.complete(externalTask)
         }catch (exception: Exception){
